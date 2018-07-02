939faaf4fe5bdd1640bc77c081a939817fb7d821
Cutlass <kwonarseus@naver.com>
2018-07-02 15:08:10 +0900

    Remove duplicate code

--- src/com/withpwn/TargetImage.java
+++ src/com/withpwn/TargetImage.java
@@ -28,11 +28,6 @@ public class TargetImage {
 
     String GetTd(int x, int y) {
         int[] colors = GetBG(x, y);
-        int clr = Image.getRGB(x, y);
-
-        colors[0] = (clr & 0x00ff0000) >> 16;
-        colors[1] = (clr & 0x0000ff00) >> 8;
-        colors[2] = clr & 0x000000ff;
 
         return String.format("            <td style=\"background:#%02x%02x%02x;\"></td>\n", colors[0], colors[1], colors[2]);
     }
