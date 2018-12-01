4d25b1efc5aebbe5736874b3a2ac9d9f67e905c6
Cutlass <kwonarseus@naver.com>
2018-12-01 21:16:04 +0900

    Output format edited. Adding graphic interface.

--- src/com/withpwn/HtmlPainter.java
+++ src/com/withpwn/HtmlPainter.java
@@ -2,59 +2,97 @@ package com.withpwn;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class HtmlPainter {
+public class HtmlPainter extends JFrame implements ActionListener {
     Scanner sc = new Scanner(System.in);
+
+    TargetImage targetImage;
+    OutputHTML outputHTML;
+    int returnVal;
 
     JFrame chooserWindow;
     JFileChooser fileChooser = new JFileChooser();
     File file;
 
+    JPanel panel = new JPanel();
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+
+    JButton button = new JButton("Target Image");
+    JButton button1 = new JButton("Output Path");
+    JButton Paint = new JButton("Paint!");
+
     HtmlPainter() throws IOException {
-        // Step 1
-        System.out.println("[*] Step 1. Select Target Image");
-        chooserWindow = new JFrame();
-        int returnVal = fileChooser.showOpenDialog(chooserWindow);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            file = fileChooser.getSelectedFile();
-        } else {
-            System.out.println("[*] Canceled");
-            System.exit(0);
+        setTitle("Html-Painter");
+        setSize(800, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button.addActionListener(this);
+        button1.addActionListener(this);
+        Paint.addActionListener(this);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("Choose Target Image"));
+        panel.add(button);
+
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        panel1.add(new JLabel("Input Output Path"));
+        panel1.add(button1);
+
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+        panel2.add(new JLabel("Paint?"));
+        panel2.add(Paint);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("[*] Step 1. Select Target Image");
+            chooserWindow = new JFrame();
+            returnVal = fileChooser.showOpenDialog(chooserWindow);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+            } else {
+                System.out.println("[*] Canceled");
+                System.exit(0);
+            }
+
+            try {
+                targetImage = new TargetImage(file);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            outputHTML = new OutputHTML(targetImage.GetBG(0, 0));
+        } else if (e.getSource() == button1) {
+            System.out.println("[*] Step 2. Select Output Location");
+            fileChooser.setFileFilter(new FileNameExtensionFilter("Html file", "html"));
+            returnVal = fileChooser.showSaveDialog(chooserWindow);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+            } else {
+                System.out.println("[*] Canceled");
+                System.exit(0);
+            }
+        } else if (e.getSource() == Paint) {
+            // Confirm
+            System.out.print("[*] Start Paint? [y/n]: ");
+            String YorN = sc.next();
+            if (YorN.equals("y") || YorN.equals("Y")) {
+                outputHTML.Insert2Buf(targetImage);
+                outputHTML.WriteInFile(file);
+            } else {
+                System.out.println("[*] Canceled");
+                System.exit(0);
+            }
+
+            System.out.println("[*] All process finished. Enjoy yourself!");
         }
-
-        TargetImage targetImage = new TargetImage(file);
-        OutputHTML outputHTML = new OutputHTML(targetImage.GetBG(0, 0));
-
-        /////////////////////////////////////////////////////////////////////////////
-
-        // Step 2
-        System.out.println("[*] Step 2. Select Output Location");
-        fileChooser.setFileFilter(new FileNameExtensionFilter("Html file", "html"));
-        returnVal = fileChooser.showSaveDialog(chooserWindow);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            file = fileChooser.getSelectedFile();
-        } else {
-            System.out.println("[*] Canceled");
-            System.exit(0);
-        }
-
-        /////////////////////////////////////////////////////////////////////////////
-
-        // Confirm
-        System.out.print("[*] Start Paint? [y/n]: ");
-        String YorN = sc.next();
-        if (YorN.equals("y") || YorN.equals("Y")) {
-            outputHTML.Insert2Buf(targetImage);
-            outputHTML.WriteInFile(file);
-        } else {
-            System.out.println("[*] Canceled");
-            System.exit(0);
-        }
-
-        System.out.println("[*] All process finished. Enjoy yourself!");
-        System.exit(0);
     }
 }
--- src/com/withpwn/OutputHTML.java
+++ src/com/withpwn/OutputHTML.java
@@ -17,15 +17,13 @@ abstract class HTML implements H {
     @Override
     public void Insert2Buf(TargetImage image) {
         for (int i = 0; i < image.height; i++) {
-            html.add("        <tr>\n");
+            html.add("    <tr>\n");
             for (int j = 0; j < image.width; j++) {
                 html.add(image.GetTd(j, i));
             }
-            html.add("        </tr>\n");
+            html.add("    </tr>\n");
         }
-        html.add("    </table>\n");
-        html.add("</body>\n");
-        html.add("</html>");
+        html.add("</table>\n");
     }
 
     @Override
@@ -35,16 +33,12 @@ abstract class HTML implements H {
 public class OutputHTML extends HTML {
     OutputHTML(int[] bgcolor) {
         html.add("<!DOCTYPE html>\n");
-        html.add("<html>\n");
-        html.add("<head>\n");
-        html.add("    <style type=\"text/css\">\n");
-        html.add(String.format("        html{background-color:#%02x%02x%02x;}\n", bgcolor[0], bgcolor[1], bgcolor[2]));
+        html.add("<style type=\"text/css\">\n");
+        html.add(String.format("    html{background-color:#%02x%02x%02x;}\n", bgcolor[0], bgcolor[1], bgcolor[2]));
         html.add("    table{margin:auto;text-align:center;border-spacing:0px;border-style:none;padding:0px;}\n");
         html.add("    th{height:100%;width:100%;}\n");
-        html.add("    </style>\n");
-        html.add("</head>\n");
-        html.add("<body>\n");
-        html.add("    <table id=\"image\">\n");
+        html.add("</style>\n");
+        html.add("<table id=\"image\">\n");
     }
 
     @Override
